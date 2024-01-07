20bb6c08227f721b052f0b33b86b8177152a1076
BashMat <bashmatred@gmail.com>
2024-01-08 01:44:46 +0300

    Fix paths

---
+++ TaskManagerApi/TaskManagerApi/AutoMapperProfile/AutoMapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using TaskManagerApi.Domain.Dtos.Project;
+using TaskManagerApi.Domain.Models;
+
+namespace TaskManagerApi.AutoMapperProfile
+{
+	public class AutoMapperProfile : Profile
+	{
+		public AutoMapperProfile()
+		{
+			CreateMap<Project, ProjectGetResponseDto>();
+			CreateMap<ProjectAddRequestDto, Project>();
+			CreateMap<ProjectUpdateRequestDto, Project>();
+		}
+	}
+}
