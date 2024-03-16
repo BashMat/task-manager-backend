068473f11b682c0354027dd798bfd7ba055d8b65
Matvey Bashlykov <bashmatred@gmail.com>
2024-03-16 18:24:04 +0300

    Remove redundant methods from interface

--- TaskManagerApi/Src/TaskManagerApi/Services/Auth/IAuthService.cs
+++ TaskManagerApi/Src/TaskManagerApi/Services/Auth/IAuthService.cs
@@ -9,9 +9,5 @@ namespace TaskManagerApi.Services.Auth
 		public Task<ServiceResponse<UserSignUpResponseDto>> SignUp(UserSignUpRequestDto requestData);
 
 		public Task<ServiceResponse<string>> LogIn(UserLogInRequestDto requestData);
-
-		public void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt);
-
-		public bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt);
 	}
 }
