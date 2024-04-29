d030db0bebc646829b52b49ec769eb4822b666d5
Matvey Bashlykov <bashmatred@gmail.com>
2024-04-29 18:45:52 +0300

    Add logging with NLog

--- TaskManagerBackend/Src/TaskManagerBackend/Program.cs
+++ TaskManagerBackend/Src/TaskManagerBackend/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using Microsoft.OpenApi.Models;
+using NLog;
+using NLog.Web;
 using Swashbuckle.AspNetCore.Filters;
 using TaskManagerBackend.Common;
 using TaskManagerBackend.DataAccess.Repositories.Board;
@@ -16,81 +18,99 @@ namespace TaskManagerBackend
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            if (builder.Configuration[ConfigurationKeys.Token] == null)
+            var logger = LogManager.Setup()
+                                   .LoadConfigurationFromAppSettings()
+                                   .GetCurrentClassLogger();
+            try
             {
-                throw new ConfigurationErrorsException("Secret key for token was not specified");
-            }
+                var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddControllers();
-
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen(options =>
-            {
-                options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                if (string.IsNullOrWhiteSpace(builder.Configuration[ConfigurationKeys.Token]))
                 {
-                    Description = "Use the Bearer scheme: \"bearer {token}\"",
-                    In = ParameterLocation.Header,
-                    Name = "Authorization",
-                    Type = SecuritySchemeType.ApiKey
+                    throw new ConfigurationErrorsException("Secret key for token was not specified");
+                }
+
+                // Add services to the container.
+                builder.Services.AddControllers();
+
+                // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+                builder.Services.AddEndpointsApiExplorer();
+                builder.Services.AddSwaggerGen(options =>
+                                               {
+                                                   options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                                                       {
+                                                           Description =
+                                                               "Use the Bearer scheme: \"bearer {token}\"",
+                                                           In = ParameterLocation.Header,
+                                                           Name = "Authorization",
+                                                           Type = SecuritySchemeType.ApiKey
+                                                       });
+                                                   options.OperationFilter<SecurityRequirementsOperationFilter>();
+                                               });
+
+                builder.Host.UseNLog();
+                builder.Services.AddScoped<IAuthService, AuthService>();
+                builder.Services.AddScoped<IBoardService, BoardService>();
+                builder.Services.AddScoped<IUserRepository, UserRepository>();
+                builder.Services.AddScoped<IBoardRepository, BoardRepository>();
+                builder.Services.AddScoped<IAuthProvider, AuthProvider>();
+
+                builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                                                        {
+                                                            ValidateIssuerSigningKey = true,
+                                                            IssuerSigningKey = 
+                                                                new SymmetricSecurityKey(Encoding.UTF8
+                                                                    .GetBytes(builder.Configuration[ConfigurationKeys.Token])),
+                                                            ValidateIssuer = false,
+                                                            ValidateAudience = false,
+                                                            ValidateLifetime = true,
+                                                            ClockSkew = TimeSpan.Zero
+                                                        };
                 });
-                options.OperationFilter<SecurityRequirementsOperationFilter>();
-            });
 
-            builder.Services.AddScoped<IAuthService, AuthService>();
-            builder.Services.AddScoped<IBoardService, BoardService>();
-            builder.Services.AddScoped<IUserRepository, UserRepository>();
-            builder.Services.AddScoped<IBoardRepository, BoardRepository>();
-            builder.Services.AddScoped<IAuthProvider, AuthProvider>();
+                builder.Services.AddCors(options =>
+                                         {
+                                             options.AddPolicy("MyDefaultPolicy",
+                                                               policy =>
+                                                               {
+                                                                   policy.AllowAnyOrigin()
+                                                                         .AllowAnyHeader()
+                                                                         .AllowAnyMethod();
+                                                               });
+                                         });
 
-            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
-            {
-                options.TokenValidationParameters = new TokenValidationParameters
+                var app = builder.Build();
+
+                // Configure the HTTP request pipeline.
+                if (app.Environment.IsDevelopment())
                 {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(
-                        Encoding.UTF8.GetBytes(builder.Configuration[ConfigurationKeys.Token])),
-                    ValidateIssuer = false,
-                    ValidateAudience = false,
-                    ValidateLifetime = true,
-                    ClockSkew = TimeSpan.Zero
-                };
-            });
+                    app.UseSwagger();
+                    app.UseSwaggerUI();
+                }
 
-            builder.Services.AddCors(options =>
-            {
-                options.AddPolicy("MyDefaultPolicy",
-                    policy =>
-                    {
-                        policy.AllowAnyOrigin()
-                        .AllowAnyHeader()
-                        .AllowAnyMethod();
-                    });
-            });
+                app.UseHttpsRedirection();
 
-            var app = builder.Build();
+                app.UseCors();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                app.UseAuthentication();
+
+                app.UseAuthorization();
+
+                app.MapControllers();
+
+                app.Run();
             }
-
-            app.UseHttpsRedirection();
-
-            app.UseCors();
-
-            app.UseAuthentication();
-
-            app.UseAuthorization();
-
-            app.MapControllers();
-
-            app.Run();
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Error occured during application initialization");
+                throw;
+            }
+            finally
+            {
+                LogManager.Shutdown();
+            }
         }
     }
 }
