7801ff233dad56a74e4004d0dfcca2ef8f75d6dc
Matvey Bashlykov <bashmatred@gmail.com>
2025-04-08 23:29:21 +0300

    Add interface for tracking service

---
+++ TaskManagerBackend/src/TaskManagerBackend.Application/Services/Tracking/ITrackingService.cs
@@ -0,0 +1,15 @@
+﻿using TaskManagerBackend.Dto.Tracking.TrackingLog;
+
+namespace TaskManagerBackend.Application.Services.Tracking;
+
+public interface ITrackingService
+{
+    #region Tracking Logs
+
+    Task<ServiceResponse<TrackingLogGetResponse>> Create(int userId, TrackingLogCreateRequest newLog);
+    Task<ServiceResponse<List<TrackingLogGetResponse>>> GetAll(int userId);
+    Task<ServiceResponse<TrackingLogGetResponse>> GetById(int id);
+    Task<ServiceResponse<List<TrackingLogGetResponse>>> Delete(int userId, int boardId);
+
+    #endregion
+}
