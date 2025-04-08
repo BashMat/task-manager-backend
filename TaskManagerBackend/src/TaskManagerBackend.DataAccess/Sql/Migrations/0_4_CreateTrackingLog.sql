52c3fe5e4710e676da8fe355dfe719b95613606d
Matvey Bashlykov <bashmatred@gmail.com>
2025-04-08 23:22:34 +0300

    Add SQL scripts to create tables for new entities

---
+++ TaskManagerBackend/src/TaskManagerBackend.DataAccess/Sql/Migrations/0_4_CreateTrackingLog.sql
@@ -0,0 +1,15 @@
+﻿create table [TrackingLog]
+(
+    [Id] int primary key identity(1, 1),
+    [Title] nvarchar(256) not null,
+    [Description] nvarchar(512),
+    [CreatedBy] int not null,
+    [CreatedAt] datetime2 not null,
+    [UpdatedBy] int not null,
+    [UpdatedAt] datetime2 not null,
+
+    constraint [TrackingLog_CreatedBy_FK] 
+    foreign key ([CreatedBy]) references [User] ([Id]),
+    constraint [TrackingLog_UpdatedBy_FK]
+    foreign key ([UpdatedBy]) references [User] ([Id]),
+)
