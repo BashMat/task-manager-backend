96993aefb48cda049181a3b4c24090b599f1eb61
Matvey Bashlykov <bashmatred@gmail.com>
2025-04-29 22:43:56 +0300

    Extract common integration test configuration into base class

--- TaskManagerBackend/tests/integration/TaskManagerBackend.IntegrationTests/Auth/AuthorizationTestBase.cs
+++ TaskManagerBackend/tests/integration/TaskManagerBackend.IntegrationTests/Auth/AuthorizationTestBase.cs
@@ -1,30 +1,12 @@
 ﻿#region Usings
 
-using Microsoft.AspNetCore.Mvc.Testing;
-using TaskManagerBackend.Tests.Common;
 using Xunit;
 
 #endregion
 
 namespace TaskManagerBackend.IntegrationTests.Auth;
 
-public class AuthorizationTestBase : CommonTestBase, IClassFixture<MsSqlTests>, IDisposable
+public class AuthorizationTestBase : IntegrationTestBase, IClassFixture<MsSqlTests>, IDisposable
 {
-    private readonly WebApplicationFactory<TaskManagerBackend.Application.Program> _webApplicationFactory;
-
-    protected TaskManagerBackendHttpClient HttpClient { get; init; }
-
-    protected AuthorizationTestBase(MsSqlTests fixture)
-    {
-        var clientOptions = new WebApplicationFactoryClientOptions();
-        clientOptions.AllowAutoRedirect = false;
-
-        _webApplicationFactory = new CustomWebApplicationFactory(fixture);
-        HttpClient = _webApplicationFactory.CreateClient(clientOptions);
-    }
-
-    public void Dispose()
-    {
-        _webApplicationFactory.Dispose();
-    }
+    protected AuthorizationTestBase(MsSqlTests fixture) : base(fixture) { }
 }
---
+++ TaskManagerBackend/tests/integration/TaskManagerBackend.IntegrationTests/IntegrationTestBase.cs
@@ -0,0 +1,30 @@
+﻿#region Usings
+
+using Microsoft.AspNetCore.Mvc.Testing;
+using TaskManagerBackend.Tests.Common;
+using Xunit;
+
+#endregion
+
+namespace TaskManagerBackend.IntegrationTests;
+
+public class IntegrationTestBase : CommonTestBase, IClassFixture<MsSqlTests>, IDisposable
+{
+    private readonly WebApplicationFactory<TaskManagerBackend.Application.Program> _webApplicationFactory;
+
+    protected TaskManagerBackendHttpClient HttpClient { get; init; }
+
+    protected IntegrationTestBase(MsSqlTests fixture)
+    {
+        var clientOptions = new WebApplicationFactoryClientOptions();
+        clientOptions.AllowAutoRedirect = false;
+
+        _webApplicationFactory = new CustomWebApplicationFactory(fixture);
+        HttpClient = _webApplicationFactory.CreateClient(clientOptions);
+    }
+
+    public virtual void Dispose()
+    {
+        _webApplicationFactory.Dispose();
+    }
+}
