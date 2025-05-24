417c2d75b6d856687a28410abdf43ded0178fde5
Matvey Bashlykov <bashmatred@gmail.com>
2025-05-24 12:20:06 +0300

    Add implicit conversion from generic type to ServiceResponse instance

--- TaskManagerBackend/src/TaskManagerBackend.Common/ServiceResponse.cs
+++ TaskManagerBackend/src/TaskManagerBackend.Common/ServiceResponse.cs
@@ -5,4 +5,12 @@ public class ServiceResponse<T>
     public T? Data { get; set; }
     public bool Success { get; set; } = true;
     public string? Message { get; set; }
+    
+    public static implicit operator ServiceResponse<T>(T? data)
+    {
+        return new ServiceResponse<T>()
+               {
+                   Data = data
+               };
+    }
 }
