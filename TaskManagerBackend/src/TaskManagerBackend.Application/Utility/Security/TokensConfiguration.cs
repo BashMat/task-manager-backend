41fe1eecd7b5309ee4cbf03447ddc89cdc16bfec
Matvey Bashlykov <bashmatred@gmail.com>
2025-07-30 21:50:25 +0300

    Add configuration setting for refresh token lifetime

--- TaskManagerBackend/src/TaskManagerBackend.Application/Utility/Security/TokensConfiguration.cs
+++ TaskManagerBackend/src/TaskManagerBackend.Application/Utility/Security/TokensConfiguration.cs
@@ -4,5 +4,7 @@ public class TokensConfiguration
 {
     public required string Secret { get; init; }
     public required string AccessTokenLifeTimeInMinutes { get; init; }
+    public required string RefreshTokenLifeTimeInMinutes { get; init; }
     public double AccessTokenLifeTimeInMinutesAsDouble => Convert.ToDouble(AccessTokenLifeTimeInMinutes);
+    public double RefreshTokenLifeTimeInMinutesAsDouble => Convert.ToDouble(RefreshTokenLifeTimeInMinutes);
 }
