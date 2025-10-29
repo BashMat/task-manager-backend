f5051beafa8f2bb7083308c2a7b0edd2402c9788
Matvey Bashlykov <bashmatred@gmail.com>
2025-10-29 22:49:48 +0300

    Rename generic type parameter

--- TaskManagerBackend/src/TaskManagerBackend.Domain/Events/IEvent.cs
+++ TaskManagerBackend/src/TaskManagerBackend.Domain/Events/IEvent.cs
@@ -1,12 +1,12 @@
 ﻿namespace TaskManagerBackend.Domain.Events;
 
-public interface IEvent<TObject>
+public interface IEvent<TEntity>
 {
     public Guid Id { get; }
     public int EntityType { get; }
     public int EntityId { get; }
     public int EntityVersion { get; }
-    public TObject Data { get; }
+    public TEntity Data { get; }
     public int DispatchedByUserId { get; }
     public DateTime DispatchedAt { get; }
     public Guid CorrelationId { get; }
