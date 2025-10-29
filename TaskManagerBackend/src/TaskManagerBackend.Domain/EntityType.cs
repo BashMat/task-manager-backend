b0564b94aad162b05b21a10f927274045a7bf51a
Matvey Bashlykov <bashmatred@gmail.com>
2025-10-29 22:13:16 +0300

    Add enumeration class to provide domain entities

---
+++ TaskManagerBackend/src/TaskManagerBackend.Domain/EntityType.cs
@@ -0,0 +1,18 @@
+﻿namespace TaskManagerBackend.Domain;
+
+public class EntityType
+{
+    public int Id { get; }
+    public string Name { get; }
+    
+    private EntityType(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+
+    public static EntityType User { get; } = new(1, "User");
+    public static EntityType TrackingLog { get; } = new(2, "TrackingLog");
+    public static EntityType TrackingLogEntryStatus { get; } = new(3, "TrackingLogEntryStatus");
+    public static EntityType TrackingLogEntry { get; } = new(4, "TrackingLogEntry");
+}
