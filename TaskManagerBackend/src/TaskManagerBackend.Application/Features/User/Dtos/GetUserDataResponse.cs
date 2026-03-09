a28e67768721403b9f825b21370054e745fae581
Matvey Bashlykov <bashmatred@yandex.ru>
2026-03-09 14:10:48 +0300

    style: Cleanup request and response DTOs

--- TaskManagerBackend/src/TaskManagerBackend.Application/Features/User/Dtos/GetUserDataResponse.cs
+++ TaskManagerBackend/src/TaskManagerBackend.Application/Features/User/Dtos/GetUserDataResponse.cs
@@ -3,5 +3,5 @@ namespace TaskManagerBackend.Application.Features.User.Dtos;
 public class GetUserDataResponse
 {
     public int Id { get; init; }
-    public string UserName { get; init; } = null!;
+    public required string UserName { get; init; }
 }
