d3f895ca8187444bdbbfe49dac01db99d4b5cd2d
Matvey Bashlykov <bashmatred@yandex.ru>
2026-03-09 03:04:00 +0300

    refactoring: Delete unused code

--- TaskManagerBackend/src/TaskManagerBackend.DataAccess/Repositories/Tracking/UserNameAndEmailData.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TaskManagerBackend.DataAccess.Repositories.Tracking;
-
-public class UserNameAndEmailData
-{
-    public string UserName { get; set; } = null!;
-    public string Email { get; set; } = null!;
-}
