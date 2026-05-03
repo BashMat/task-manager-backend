4148a6f16420b088bb63d9ef0c36f75e84886158
Matvey Bashlykov <bashmatred@yandex.ru>
2026-05-03 13:24:41 +0300

    refactoring: Use IEntityTypeConfiguration instances to configure EF Core model entities

---
+++ TaskManagerBackend/src/TaskManagerBackend.DataAccess/Database/Models/TrackingLogEntryStatusConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace TaskManagerBackend.DataAccess.Database.Models;
+
+public class TrackingLogEntryStatusConfiguration : IEntityTypeConfiguration<TrackingLogEntryStatus>
+{
+    public void Configure(EntityTypeBuilder<TrackingLogEntryStatus> builder)
+    {
+        builder.HasKey(e => e.Id).HasName("TrackingLogEntryStatus_PK");
+
+        builder.ToTable("TrackingLogEntryStatus");
+
+        builder.Property(e => e.Description).HasMaxLength(512);
+        builder.Property(e => e.Title).HasMaxLength(256);
+
+        builder.HasOne(d => d.CreatedByNavigation)
+               .WithMany(p => p.TrackingLogEntryStatusCreatedByNavigations)
+               .HasForeignKey(d => d.CreatedBy)
+               .OnDelete(DeleteBehavior.ClientSetNull)
+               .HasConstraintName("TrackingLogEntryStatus_CreatedBy_FK");
+
+        builder.HasOne(d => d.TrackingLog)
+               .WithMany(p => p.TrackingLogEntryStatuses)
+               .HasForeignKey(d => d.TrackingLogId)
+               .OnDelete(DeleteBehavior.ClientSetNull)
+               .HasConstraintName("TrackingLogEntryStatus_TrackingLogId_FK");
+
+        builder.HasOne(d => d.UpdatedByNavigation)
+               .WithMany(p => p.TrackingLogEntryStatusUpdatedByNavigations)
+               .HasForeignKey(d => d.UpdatedBy)
+               .OnDelete(DeleteBehavior.ClientSetNull)
+               .HasConstraintName("TrackingLogEntryStatus_UpdatedBy_FK");
+    }
+}
