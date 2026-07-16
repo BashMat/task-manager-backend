dafb8044d553f6c1ddc85ac53e9f4c1c4783d26c
Matvey Bashlykov <bashmatred@yandex.ru>
2026-07-16 14:44:29 +0300

    feat: Add use case specific Update API to Edit Tracking Log Entry

---
+++ TaskManagerBackend/src/TaskManagerBackend.Domain/Features/Tracking/TrackingLogEntry/TrackingLogEntryEntity.cs
@@ -0,0 +1,80 @@
+﻿#region
+
+using TaskManagerBackend.Domain.Shared.Data;
+using TaskManagerBackend.Domain.Shared.Entities;
+
+#endregion
+
+namespace TaskManagerBackend.Domain.Features.Tracking.TrackingLogEntry;
+
+public class TrackingLogEntryEntity : IAuditedEntity
+{
+    public TrackingLogEntryEntity(int id,
+                                  StringAttribute title,
+                                  int trackingLogEntryStatusId,
+                                  StringAttribute? description,
+                                  decimal orderIndex,
+                                  int createdBy,
+                                  DateTime createdAt,
+                                  int updatedBy,
+                                  DateTime updatedAt)
+    {
+        Id = id;
+        Title = title;
+        TrackingLogEntryStatusId = trackingLogEntryStatusId;
+        Description = description;
+        OrderIndex = orderIndex;
+        CreatedBy = createdBy;
+        CreatedAt = createdAt;
+        UpdatedBy = updatedBy;
+        UpdatedAt = updatedAt;
+    }
+    
+    public int Id { get; }
+    public StringAttribute Title { get; private set; }
+    public int TrackingLogEntryStatusId { get; private set; }
+    public StringAttribute? Description { get; private set; }
+    public decimal OrderIndex { get; private set; }
+    public int CreatedBy { get; }
+    public DateTime CreatedAt { get; }
+    public int UpdatedBy { get; private set; }
+    public DateTime UpdatedAt { get; private set; }
+
+    public void RenameToByUser(StringAttribute value,
+                               int userId,
+                               DateTime dateTime)
+    {
+        if (Title.Value != value.Value)
+        {
+            Title = value;
+            UpdatedBy = userId;
+            UpdatedAt = dateTime;
+        }
+    }
+    
+    public void ChangeStatusToByUser(int value,
+                                     int userId,
+                                     TrackingLogEntryEntity? targetStatusTopmostTrackingLogEntry,
+                                     DateTime dateTime)
+    {
+        if (TrackingLogEntryStatusId != value)
+        {
+            TrackingLogEntryStatusId = value;
+            OrderIndex = OrderIndexCalculator.CalculateTargetOrderIndex(targetStatusTopmostTrackingLogEntry?.OrderIndex);
+            UpdatedBy = userId;
+            UpdatedAt = dateTime;
+        }
+    }
+    
+    public void EditDescriptionToByUser(StringAttribute? value,
+                                        int userId,
+                                        DateTime dateTime)
+    {
+        if (Description?.Value != value?.Value)
+        {
+            Description = value;
+            UpdatedBy = userId;
+            UpdatedAt = dateTime;
+        }
+    }
+}
