64ae17da4fc3539d0d55012d92cc6736791b2510
Matvey Bashlykov <bashmatred@yandex.ru>
2026-09-10 20:27:27 +0300

    feat: Add OrderIndexCalculator

---
+++ TaskManagerBackend/src/TaskManagerBackend.Domain/Features/Tracking/TrackingLogEntry/OrderIndexCalculator.cs
@@ -0,0 +1,22 @@
+﻿using TaskManagerBackend.Domain.Shared.Validation;
+
+namespace TaskManagerBackend.Domain.Features.Tracking.TrackingLogEntry;
+
+public static class OrderIndexCalculator
+{
+    public static decimal CalculateTargetOrderIndex(decimal? currentMinimum)
+    {
+        if (currentMinimum is null)
+        {
+            return (decimal)Constants.MinOrderIndex;
+        }
+
+        if (currentMinimum < (decimal)Constants.MinOrderIndex ||
+            currentMinimum > (decimal)Constants.MaxOrderIndex)
+        {
+            throw new ArgumentException(null, nameof(currentMinimum));
+        }
+
+        return Math.Round(currentMinimum.Value / 2, 2);
+    }
+}
---
+++ TaskManagerBackend/tests/unit/TaskManagerBackend.Domain.Tests/OrderIndexCalculator/WhenCalculatingTargetOrderIndex.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace TaskManagerBackend.Domain.Tests.OrderIndexCalculator;
+
+public class WhenCalculatingTargetOrderIndex : OrderIndexCalculatorTestBase
+{
+    [Theory]
+    [MemberData(nameof(GetValidTestData))]
+    public void OrderedIndexIsCalculatedCorrectlyForValidInputData(decimal? input, decimal expectedValue)
+    {
+        decimal actualValue = Features.Tracking.TrackingLogEntry.OrderIndexCalculator.CalculateTargetOrderIndex(input);
+
+        actualValue.Should().Be(expectedValue);
+    }
+    
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(-0.1)]
+    [InlineData(-0.0000000000000000000000000001)]
+    [InlineData(100_000.000000001)]
+    [InlineData(100_001)]
+    public void ArgumentExceptionIsThrownIfArgumentIsOutOfRange(decimal input)
+    { 
+        Func<decimal> action = () => Features.Tracking.TrackingLogEntry.OrderIndexCalculator.CalculateTargetOrderIndex(input);
+
+        action.Should().ThrowExactly<ArgumentException>();
+    }
+}
